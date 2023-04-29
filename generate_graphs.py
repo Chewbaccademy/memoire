fee05eb85e1ea9e5336523a4b61fe53617dc78d5
Chewbaccademy <kilian.debraux@gmail.com>
2023-04-29 09:20:50 +0200

    modif_generate_graphs

--- generate_graphs.py
+++ generate_graphs.py
@@ -4,13 +4,14 @@ import seaborn as sns
 import glob
 import json
 import pandas as pd
+import numpy as np
 
 data = []
 
 files = glob.glob('results/*_agents_data.json')
 
 df = pd.DataFrame(columns=["time_by_distance", "time_wo_stop", "simulation_id"])
-
+raw = ""
 cpt = 0
 for file in files:
     nom_simulation = "simulation_" + str(cpt+1)
@@ -19,13 +20,21 @@ for file in files:
         data_kilian = json.loads(raw)
         data.append({"simulation": json.loads(raw)})
         for index, agent in enumerate([a for a in data_kilian if 'Agent' in a]):
-            df.loc[cpt * 15 + index] = [data_kilian[agent]["time_by_distance"], data_kilian[agent]["time_wo_stop"], nom_simulation]
+            df.loc[cpt * 60 + index] = [data_kilian[agent]["time_by_distance"], (data_kilian[agent]["time_by_distance"] - data_kilian[agent]["time_wo_stop"]) / data_kilian[agent]["time_wo_stop"], nom_simulation]
     cpt += 1
         
 def mean(data:list):
     return sum(data)/len(data)
 
-print(df.to_string())
+def sort_func(x):
+    return int(str(x).split('_')[1])
+
+dataf = df.groupby("simulation_id").mean()
+print(dataf)
+
+x = dataf["time_by_distance"]
+y = dataf["time_wo_stop"]
+
 
 # x = []
 # y = []
@@ -38,7 +47,39 @@ print(df.to_string())
 # # y = [d['mean_time_wo_stop'] for d in refined_data]
 
 sns.scatterplot(data=df, x="time_by_distance", y="time_wo_stop", hue="simulation_id")
+plt.plot([0.1, 1.15], [0, 10], color='red', linestyle='-', linewidth=2)
 plt.show()
+
+df["best_way"] = df["time_by_distance"] == df["time_wo_stop"]
+
+df2 = pd.DataFrame(index=df["simulation_id"].unique())
+df2["agent_best"] = df[df["best_way"] == 1].groupby("simulation_id").count()["best_way"]
+df2["agent_not_best"] = df[df["best_way"] == 0].groupby("simulation_id").count()["best_way"]
+print(df2)
+
+d = {"agent_did_best": df2["agent_best"], "pct_agent_did_best": (df2["agent_best"] / (df2["agent_best"] + df2["agent_not_best"]) * 100)}
+dataf = pd.DataFrame(d)
+dataf.to_csv("results/stats_agent_did_best.csv")
+# print(dataf)
+
+c = {
+    "agent_did_best": df2["agent_best"],
+    "agent_did_not_best": df2["agent_not_best"]
+}
+
+x = tuple(df2.index)
+
+fig, ax = plt.subplots()
+bottom = np.zeros(20)
+
+for i, row in c.items():
+    p = ax.bar(x, np.array(row), 0.5, label=i, bottom=bottom)
+    bottom += row
+
+ax.set_title("Nombre d'agents ayant réalisé le temps attendu par simulation")
+plt.xticks(rotation = 45)
+
+# plt.show()
 
 plt.rcParams["figure.figsize"] = (9,5)
 
@@ -133,10 +174,7 @@ for simulation in data:
             total_speed += simulation['simulation'][info]["total_distance"] / simulation['simulation'][info]["total_time"]
     y.append(total_speed/nb_agents)
 
-print(x)
-print(y)
-
 plt.barh(x, y)
 plt.xlabel("Vitesse (m/s)")
 plt.title("Vitesse moyenne des agents par simulation")
-plt.show()
+# plt.show()
